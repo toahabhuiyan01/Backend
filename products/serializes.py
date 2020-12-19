fc0a9d84e1a5ce7126026393177ac196b4755577
toahabhuiyan01 <toahabhuiyan@gmail.com>
2020-12-19 18:20:37 +0600

    first

---
+++ products/serializes.py
@@ -0,0 +1,58 @@
+from .models import *
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+
+class CategorySerializer(ModelSerializer):
+    random_photo = SerializerMethodField()
+
+    def get_random_photo(self, obj):
+        try:
+            return obj.products.first().photo
+        except:
+            return "error"
+        
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name',
+            'random_photo'
+        )
+
+class SellerSerializer(ModelSerializer):
+    class Meta:
+        model = Seller
+        fields = ( 
+            'id',
+            'name'
+        )
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ( 
+            'id',
+            'photo',
+            'price',
+            'title',
+            'category',
+            'seller',
+            'photo'
+        )
+
+
+class ProductsAllInfoSerializer(ModelSerializer):
+    category    = CategorySerializer()
+    seller      = SellerSerializer()
+    
+    class Meta:
+        model = Product
+        fields = ( 
+            'id',
+            'photo',
+            'price',
+            'title',
+            'category',
+            'seller',
+            'photo'
+        )
